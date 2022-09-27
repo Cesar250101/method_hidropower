ec815935dd215c67d79649f7e22a7f5e85ef9cd7
Cesar250101 <cesar250101@outlook.com>
2022-09-27 12:53:46 -0300

    informe de salida

--- models/informesalida.py
+++ models/informesalida.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from email.policy import default
 from odoo import models, fields, api
 
 class Otros(models.Model):
@@ -9,6 +10,28 @@ class Otros(models.Model):
     name = fields.Char(string='Nombre')
     informe_salida_id = fields.Many2one(comodel_name='method_hidropower.informe_salida', string='Informe de Salida')
     
+class SelloTapaPrensa1(models.Model):
+    _name = 'method_hidropower.sello_tapa_prensa_1'
+    _description = 'Sello tapa prensa 1'
+
+    name = fields.Char(string='Nombre')
+    informe_salida_id = fields.Many2one(comodel_name='method_hidropower.informe_salida', string='Informe de Salida')
+
+class SelloPiston(models.Model):
+    _name = 'method_hidropower.sello_piston'
+    _description = 'Sello Pistón'
+
+    name = fields.Char(string='Nombre')
+    informe_salida_id = fields.Many2one(comodel_name='method_hidropower.informe_salida', string='Informe de Salida')
+
+class SelloPiston(models.Model):
+    _name = 'method_hidropower.concluciones'
+    _description = 'Informe de Salida - Concluciones'
+
+    name = fields.Char(string='Nombre')
+    informe_salida_id = fields.Many2one(comodel_name='method_hidropower.informe_salida', string='Informe de Salida')
+
+
 class InformeSalida(models.Model):
     _name = 'method_hidropower.informe_salida'
     _description = 'Informe de Salida'
@@ -35,63 +58,23 @@ class InformeSalida(models.Model):
     img_2_1=fields.Binary('Imagen sello tapa prensa 1')
     img_2_2=fields.Binary('Imagen sello tapa prensa 2')
     img_2_3=fields.Binary('Imagen sello tapa prensa 3')
-    select_2_1 = fields.Selection(string='sello tapa prensa 1', 
-                    selection=[('1', 'Se reaiiza pulido y lavada de Tapa prensa'), 
-                    ('2', 'Se realiza instalacion de sellos a tapa prensa'),
-                    ('3', 'Se recupera pista de sello vastago a tapa prensa'),
-                    ('4', 'Se instala sellos k17 a tapa prensa'),
-                    ('5', 'Se recuperan hilos de tapa prensa'),
-                    ('6', 'Se fabrica tapa prensa según muestra'),
-                    ('7', 'Se fabrica tapa prensa según plano'),
-                    ])
-    select_2_2 = fields.Selection(string='sello tapa prensa 2', 
-                    selection=[('1', 'Se reaiiza pulido y lavada de Tapa prensa'), 
-                    ('2', 'Se realiza instalacion de sellos a tapa prensa'),
-                    ('3', 'Se recupera pista de sello vastago a tapa prensa'),
-                    ('4', 'Se instala sellos k17 a tapa prensa'),
-                    ('5', 'Se recuperan hilos de tapa prensa'),
-                    ('6', 'Se fabrica tapa prensa según muestra'),
-                    ('7', 'Se fabrica tapa prensa según plano'),
-                    ])
-    select_2_3 = fields.Selection(string='sello tapa prensa 3', 
-                    selection=[('1', 'Se reaiiza pulido y lavada de Tapa prensa'), 
-                    ('2', 'Se realiza instalacion de sellos a tapa prensa'),
-                    ('3', 'Se recupera pista de sello vastago a tapa prensa'),
-                    ('4', 'Se instala sellos k17 a tapa prensa'),
-                    ('5', 'Se recuperan hilos de tapa prensa'),
-                    ('6', 'Se fabrica tapa prensa según muestra'),
-                    ('7', 'Se fabrica tapa prensa según plano'),
-                    ])
+
+    sello_tapa_prensa_ids = fields.One2many('method_hidropower.sello_tapa_prensa_1', 'informe_salida_id', string='Sello Tapa Prensa 1')
     img_3_1=fields.Binary('Imagen sello pistón 1')
     img_3_2=fields.Binary('Imagen sello pistón 2')
     img_3_3=fields.Binary('Imagen sello pistón 3')
-    select_3_1 = fields.Selection(string='Sello pistón 1', 
-                    selection=[('1', 'Se reaiiza pulido y lavada de piston'), 
-                    ('2', 'Se realiza instalacion de sellos a piston'),
-                    ('3', 'Se recupera pista diametro exterior de piston'),
-                    ('5', 'Se recuperan hilos diametro interior de piston'),
-                    ('5', 'Se recuperan hilos de tapa prensa'),
-                    ('6', 'Se fabrica piston según muestra'),
-                    ('7', 'Se fabrica piston según plano'),
-                    ])
-    select_3_2 = fields.Selection(string='Sello pistón 2', 
-                    selection=[('1', 'Se reaiiza pulido y lavada de piston'), 
-                    ('2', 'Se realiza instalacion de sellos a piston'),
-                    ('3', 'Se recupera pista diametro exterior de piston'),
-                    ('5', 'Se recuperan hilos diametro interior de piston'),
-                    ('5', 'Se recuperan hilos de tapa prensa'),
-                    ('6', 'Se fabrica piston según muestra'),
-                    ('7', 'Se fabrica piston según plano'),
-                    ])
-    select_3_3 = fields.Selection(string='Sello pistón 3', 
-                    selection=[('1', 'Se reaiiza pulido y lavada de piston'), 
-                    ('2', 'Se realiza instalacion de sellos a piston'),
-                    ('3', 'Se recupera pista diametro exterior de piston'),
-                    ('5', 'Se recuperan hilos diametro interior de piston'),
-                    ('5', 'Se recuperan hilos de tapa prensa'),
-                    ('6', 'Se fabrica piston según muestra'),
-                    ('7', 'Se fabrica piston según plano'),
-                    ])
+
+    
+    sello_piston_ids = fields.One2many('method_hidropower.sello_piston', 'informe_salida_id', string='Sello Pistón')
+    # sello_piston_ids= fields.Selection(string='Sello pistón 1', 
+    #                 selection=[('1', 'Se reaiiza pulido y lavada de piston'), 
+    #                 ('2', 'Se realiza instalacion de sellos a piston'),
+    #                 ('3', 'Se recupera pista diametro exterior de piston'),
+    #                 ('5', 'Se recuperan hilos diametro interior de piston'),
+    #                 ('5', 'Se recuperan hilos de tapa prensa'),
+    #                 ('6', 'Se fabrica piston según muestra'),
+    #                 ('7', 'Se fabrica piston según plano'),
+    #                 ])
     img_4_1=fields.Binary('Fotografia 1 Instalacion prensa piston')
     img_4_2=fields.Binary('Fotografia 2 Armado de Cilindro')
     img_4_3=fields.Binary('Fotografia 3 Torque de perno')
@@ -105,11 +88,16 @@ class InformeSalida(models.Model):
     txt_7_1 = fields.Text(string='Texto Presión interna área base pistón de cilindro.')
 
     img_8_1=fields.Binary('Imagen presión en cilindro junto a reloj comparador')
-    txt_8_1 = fields.Text(string='Texto Imagen presión en cilindro junto a reloj comparador')
+    txt_8_1 = fields.Text(string='Texto Imagen presión en cilindro junto a reloj comparador',
+                                defaul='Cilindro no presenta fuga de aceite - No presenta by pass - No presenta diferencia en medidas de en reloj comparador * Dejar Opcion de texto abierto')
 
     img_9_1=fields.Binary('Cierre de proceso')
-    txt_9_1 = fields.Text(string='Texto Cierre de proceso')
+    txt_9_1 = fields.Text(string='Texto Cierre de proceso',default='Texto Fijo: Equipo cumple las prueba hidraulica.')
 
-    conclusion = fields.Text(string='Conclusión')
+    img_10_1=fields.Binary('Gráfico test senso node parker')
+    txt_10_1 = fields.Text(string='Test senso node parker',default='Se agregan graficos del test de senso node parker con registro de las presiones por segundo.')
+
+
+    conclucion_ids = fields.One2many('method_hidropower.concluciones', 'informe_salida_id', string='Sello Pistón')
 
 
