7d351059a9fb908fc9eec33fe0b20af2b7993e59
Cesar250101 <cesar250101@outlook.com>
2022-10-25 15:56:25 -0300

    RMA en Picking

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import models
 from . import informesalida
+from . import picking
---
+++ models/picking.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from email.policy import default
+from odoo import models, fields, api
+
+class NotaVenta(models.Model):
+    _inherit = 'stock.picking'
+
+    rma_id = fields.Many2one('repair.order',string='OTH')
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        domain={'rma_id':[('partner_id','=',self.partner_id.id),('state','!=','done')]}
+        return {'domain':domain}
